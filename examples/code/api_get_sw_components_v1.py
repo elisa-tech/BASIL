465ee0c1f49e401b8438ead3c28d31be4a9365f9
Luigi Pellecchia <lpellecc@redhat.com>
2024-03-22 15:36:02 +0100

    Added 2 python scripts as an example source code as reference documentation to create sw requirements
    Signed-off-by: Luigi Pellecchia <lpellecc@redhat.com>

---
+++ examples/code/api_get_sw_components_v1.py
@@ -0,0 +1,43 @@
+import json
+import os
+import traceback
+import urllib.request
+
+API_URL = 'http://localhost:5005'
+
+def api_get_sw_components() -> list:
+    """
+    Return list of Sw Components from BASIL Api
+    """
+    ret = []
+    try:
+        response = urllib.request.urlopen(f'{API_URL}/apis')
+        content = json.loads(response.read().decode('utf-8'))
+        if isinstance(content, list):
+            print(f'Sw Components count: {len(content)}')
+            ret = content
+        else:
+            print('Error: The return value is not a list')
+    except Exception as exp:
+        print(traceback.format_exc())
+    return ret
+
+def write_sw_components_to_file(content) -> int:
+    """
+    Write content as JSON to a file
+    """
+    ret = 0
+    JSON_FILE = 'api.json'
+    try:
+        f = open(JSON_FILE, 'w')
+        json.dump(content, f, indent=2)
+        f.close()
+        if os.path.exists(JSON_FILE):
+            print(f'File {JSON_FILE} exists')
+            ret = 1
+    except Exception as exp:
+        print(traceback.format_exc())
+    return ret
+
+if __name__ == '__main__':
+    write_sw_components_to_file(api_get_sw_components())
---
+++ examples/code/api_get_sw_components_v2.py
@@ -0,0 +1,47 @@
+import json
+import os
+import sys
+import traceback
+import urllib.request
+
+API_URL = 'http://localhost:5005'
+
+def api_get_sw_components(filter) -> list:
+    """
+    Return list of Sw Components from BASIL Api
+    """
+    ret = []
+    try:
+        response = urllib.request.urlopen(f'{API_URL}/apis?search={filter}')
+        content = json.loads(response.read().decode('utf-8'))
+        if isinstance(content, list):
+            print(f'Sw Components count: {len(content)}')
+            ret = content
+        else:
+            print('Error: The return value is not a list')
+    except Exception as exp:
+        print(traceback.format_exc())
+    return ret
+
+def write_sw_components_to_file(content) -> int:
+    """
+    Write content as JSON to a file
+    """
+    ret = 0
+    JSON_FILE = 'api.json'
+    try:
+        f = open(JSON_FILE, 'w')
+        json.dump(content, f, indent=2)
+        f.close()
+        if os.path.exists(JSON_FILE):
+            print(f'File {JSON_FILE} exists')
+            ret = 1
+    except Exception as exp:
+        print(traceback.format_exc())
+    return ret
+
+if __name__ == '__main__':
+    filter = ''
+    if len(sys.argv) > 1:
+        filter = sys.argv[1]
+    write_sw_components_to_file(api_get_sw_components(filter))

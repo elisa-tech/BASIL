9d451c694079adb263ec80ddb52012112ad4dbc3
nopeppermint <3403218+nopeppermint@users.noreply.github.com.>
2025-01-24 00:11:08 +0100

    fix typos, put volume creating in a loop

--- run_custom_demo.sh
+++ run_custom_demo.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 
-echo -e "################################"
-echo -e "### prepairing BASIL Demo ###"
-echo -e "### kill all running podman container ###"
+echo -e "###################################################################"
+echo -e "### prepairing BASIL Demo"
+echo -e "### kill all running podman container\n"
 podman stop --all
 
 api_server_url=localhost
@@ -10,66 +10,71 @@ api_port=5000
 app_port=9000
 admin_pw=1234
 
-echo -e "\n### api_server_url = $api_server_url ###"
-echo -e "### api port = $api_port ###"
-echo -e "### app port = $app_port ###"
-echo -e "### admin pw = $admin_pw ###\n"
+echo -e "\n### api_server_url = $api_server_url"
+echo -e "### api port = $api_port"
+echo -e "### app port = $app_port"
+echo -e "### admin pw = $admin_pw"
 
-echo -e "################################"
-echo -e "### building api container ###\n"
+echo -e "\n###################################################################"
+echo -e "### building api container\n"
 
 podman build \
   --build-arg="ADMIN_PASSWORD=$admin_pw" \
   --build-arg="API_PORT=$api_port" \
   -f Containerfile-api \
-  -t basil-api-image-default .
+  -t basil-api-image .
 
-echo -e "################################"
-echo -e "### building app container ###\n"
+echo -e "\n###################################################################"
+echo -e "### building app container\n"
 
 podman build \
   --build-arg="API_ENDPOINT=http://$api_server_url:$api_port" \
   --build-arg="APP_PORT=$app_port" \
   -f Containerfile-app \
-  -t basil-app-image-default .
+  -t basil-app-image .
 
-echo -e "################################"
-echo -e "### create and mount volumes ###\n"
+echo -e "\n###################################################################"
+echo -e "### create and mount volumes\n"
 
-podman volume create basil-db-vol
-podman volume create basil-ssh-keys-vol
-podman volume create basil-tmt-logs-vol
+list='basil-db-vol basil-ssh-keys-vol basil-tmt-logs-vol'
+for element in $list;
+do
+    exists="$(podman volume exists "${element}")"   # check if volume already exists
+    if [ "${exists}" ]; then
+       echo "volume $element does not exist"
+       podman volume create "${element}"
+    else
+       echo "volume "${element}" already exists"
+    fi
+    podman volume mount "${element}"
+done
 
-podman volume mount basil-db-vol
-podman volume mount basil-ssh-keys-vol
-podman volume mount basil-tmt-logs-vol
-
-echo -e "################################"
-echo -e "### start api container ###\n"
-
-podman run \
--d \
---privileged \
---network=host \
-basil-api-image-default
-
-echo -e "################################"
-echo -e "### start app container ###\n"
+echo -e "\n###################################################################"
+echo -e "### start api container\n"
 
 podman run \
--d \
---privileged \
---network=host \
--v "basil-db-vol:/BASIL-API/db/sqlite3" \
--v "basil-ssh-keys-vol:/BASIL-API/api/ssh_keys" \
--v "basil-tmt-logs-vol:/var/tmp/tmt" \
-basil-app-image-default
+   -d \
+   --privileged \
+   --network=host \
+   basil-api-image
 
-echo -e "################################"
-echo -e "### list running containers ###\n"
+echo -e "\n###################################################################"
+echo -e "### start app container\n"
+
+podman run \
+   -d \
+   --privileged \
+   --network=host \
+   -v "basil-db-vol:/BASIL-API/db/sqlite3" \
+   -v "basil-ssh-keys-vol:/BASIL-API/api/ssh_keys" \
+   -v "basil-tmt-logs-vol:/var/tmp/tmt" \
+   basil-app-image
+
+echo -e "\n###################################################################"
+echo -e "### list running containers\n"
 
 podman ps
 
-echo -e "################################"
-echo -e "### start now the app via chrome browser: http://$api_server_url:$app_port ###\n"
+echo -e "\n###################################################################"
+echo -e "### start now the app via chrome browser: http://$api_server_url:$app_port\n"
 
--- run_demo.sh
+++ run_demo.sh
@@ -1,59 +1,66 @@
 #/bin/sh
 
-echo -e "################################"
-echo -e "### prepairing BASIL Demo ###\n"
+echo -e "\n###################################################################"
+echo -e "### prepairing BASIL Demo"
+echo -e "### kill all running podman container\n"
+podman stop --all
 
-echo -e "################################"
-echo -e "### pbuilding api container ###\n"
+echo -e "\n###################################################################"
+echo -e "### building api container\n"
 
 podman build \
   -f Containerfile-api \
   -t basil-api-image-default .
 
-echo -e "################################"
-echo -e "### pbuilding app container ###\n"
+echo -e "\n###################################################################"
+echo -e "### building app container\n"
 
 podman build \
   -f Containerfile-app \
   -t basil-app-image-default .
 
-echo -e "################################"
-echo -e "### pcreate and mount volumes ###\n"
+echo -e "\n###################################################################"
+echo -e "### create and mount volumes\n"
 
-podman volume create basil-db-vol
-podman volume create basil-ssh-keys-vol
-podman volume create basil-tmt-logs-vol
+list='basil-db-vol basil-ssh-keys-vol basil-tmt-logs-vol'
+for element in $list;
+do
+    exists="$(podman volume exists "${element}")"   # check if volume already exists
+    if [ "${exists}" ]; then
+       echo "volume $element does not exist"
+       podman volume create "${element}"
+    else
+       echo "volume "${element}" already exists"
+    fi
+    podman volume mount "${element}"
+done
 
-podman volume mount basil-db-vol
-podman volume mount basil-ssh-keys-vol
-podman volume mount basil-tmt-logs-vol
-
-echo -e "################################"
-echo -e "### pstart api container ###\n"
-
-podman run \
--d \
---privileged \
---network=host \
-basil-api-image-default
-
-echo -e "################################"
-echo -e "### pstart app container ###\n"
+echo -e "\n###################################################################"
+echo -e "### start api container\n"
 
 podman run \
--d \
---privileged \
---network=host \
--v "basil-db-vol:/BASIL-API/db/sqlite3" \
--v "basil-ssh-keys-vol:/BASIL-API/api/ssh_keys" \
--v "basil-tmt-logs-vol:/var/tmp/tmt" \
-basil-app-image-default
+   -d \
+   --privileged \
+   --network=host \
+   basil-api-image-default
 
-echo -e "################################"
-echo -e "### plist running containers ###\n"
+echo -e "\n###################################################################"
+echo -e "### start app container\n"
+
+podman run \
+   -d \
+   --privileged \
+   --network=host \
+   -v "basil-db-vol:/BASIL-API/db/sqlite3" \
+   -v "basil-ssh-keys-vol:/BASIL-API/api/ssh_keys" \
+   -v "basil-tmt-logs-vol:/var/tmp/tmt" \
+   basil-app-image-default
+
+echo -e "\n###################################################################"
+echo -e "### list running containers\n"
 
 podman ps
 
-echo -e "################################"
-echo -e "### start now the app via chrome browser: http://localhost:9000/ ###\n"
+echo -e "\n###################################################################"
+echo -e "### start now the app via chrome browser: http://localhost:9000/\n"
 

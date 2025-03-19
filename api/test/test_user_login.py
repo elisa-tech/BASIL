0e26d032684f864942eaca2ad6ecf01195cc68ef
AliaksandrKustsinski <aliaksandr.kustsinski@promwad.com>
2025-03-19 19:39:46 +0300

    API app UT: use constants for DB name and URL
    Signed-off-by: AliaksandrKustsinski <aliaksandr.kustsinski@promwad.com>

--- api/test/test_user_login.py
+++ api/test/test_user_login.py
@@ -6,6 +6,8 @@ import db.models.init_db as init_db
 from db.models.user import UserModel
 
 
+_USER_LOGIN_URL = '/user/login'
+_DB_NAME = 'test.db'
 _UT_USER_NAME = 'ut_user_name'
 _UT_USER_EMAIL = 'ut_user_email'
 _UT_USER_PASSWORD = 'ut_user_password'
@@ -23,9 +25,8 @@ def client():
 
 @pytest.fixture(scope="module", autouse=True)
 def test_client_db():
-    db_name = api.get_db()
-    init_db.initialization(db_name=db_name)
-    dbi = db_orm.DbInterface(db_name)
+    init_db.initialization(db_name=_DB_NAME)
+    dbi = db_orm.DbInterface(_DB_NAME)
 
     ut_test_user = UserModel(_UT_USER_NAME, _UT_USER_EMAIL, _UT_USER_PASSWORD, _UT_USER_ROLE)
     dbi.session.add(ut_test_user)
@@ -39,7 +40,7 @@ def test_client_db():
 def test_user_login_post_ok(client):
     """ Nominal test for the registered user """
     user_data = {'email': _UT_USER_EMAIL, 'password': _UT_USER_PASSWORD}
-    response = client.post("/user/login", json=user_data)
+    response = client.post(_USER_LOGIN_URL, json=user_data)
     assert response.status_code == 200
 
 
@@ -50,19 +51,19 @@ def test_user_login_post_ok(client):
 def test_user_login_post_unauthorized(client, email, password):
     """ Nominal test for not registered users """
     user_data = {'email': email, 'password': password}
-    response = client.post("/user/login", json=user_data)
+    response = client.post(_USER_LOGIN_URL, json=user_data)
     assert response.status_code == 401
 
 
 def test_user_login_post_wrong_credentials(client):
     """ Nominal test with incorrect password """
     user_data = {'email': _UT_USER_EMAIL, 'password': _UT_USER_PASSWORD + '_wrong_pwd'}
-    response = client.post("/user/login", json=user_data)
+    response = client.post(_USER_LOGIN_URL, json=user_data)
     assert response.status_code == 400
 
 
 def test_user_login_post_bad_request(client):
     """ Off-nominal test with incorrect payload """
     user_data = {'wrong_data': "no_email", 'password': "dummy_user"}
-    response = client.post("/user/login", json=user_data)
+    response = client.post(_USER_LOGIN_URL, json=user_data)
     assert response.status_code == 400

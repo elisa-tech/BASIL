de597575adedd994f3e202b217862b81b58d5338
Luigi <63158371+pellecchialuigi@users.noreply.github.com>
2025-08-25 15:37:18 +0200

    Update sqlite_1_7_1.sql (#186)
    Fix migration commands to update the apis_history table in accordance with changes made to apis table

--- db/models/migration/sqlite_1_7_1.sql
+++ db/models/migration/sqlite_1_7_1.sql
@@ -1,4 +1,5 @@
 ALTER TABLE apis ADD COLUMN write_permission_requests TEXT DEFAULT '';
+ALTER TABLE apis_history ADD COLUMN write_permission_requests TEXT DEFAULT '';
 ALTER TABLE notifications ADD COLUMN for_owners INTEGER DEFAULT 0;
 ALTER TABLE notifications ADD COLUMN user_ids TEXT DEFAULT '';
 UPDATE notifications SET read_by = '[' || REPLACE(read_by, ',', '][') || ']';

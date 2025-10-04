651e1080b725cced6c69e1843a54fb654a072d72
Luigi Pellecchia <lpellecc@redhat.com>
2025-10-04 10:45:40 +0200

    Avoid field not in the request_data User Signin
    Signed-off-by: Luigi Pellecchia <lpellecc@redhat.com>

--- api/api.py
+++ api/api.py
@@ -5688,7 +5688,7 @@ class UserSignin(Resource):
         # Validate fields constraints
         filed_constraints = UserModel.get_field_constraints()
         for field, constraint in filed_constraints.items():
-            if "max_length" in constraint:
+            if "max_length" in constraint and field in request_data:
                 if constraint["max_length"] is not None and len(request_data[field]) > constraint["max_length"]:
                     return f"{field} must be less than {constraint['max_length']} characters", BAD_REQUEST_STATUS
 

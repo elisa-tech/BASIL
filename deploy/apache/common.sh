5cfac055e503fcff29fd696128f0960df580c967
Stefan Pofahl <65159315+StefanPofahl@users.noreply.github.com>
2026-01-08 15:18:53 +0100

    BASIL Deploy Scripts for Apache2 Server (#221)
    * function add_port_to_listen() improved
    
    Signed-off-by: stefan_pofahl <stefan@pofahl.de>
    
    * run.sh improved, check if root, change to script folder, default value for 'BASIL_RESET_POSTGRES_CLUSTERS' is 0 now.
    
    Signed-off-by: stefan_pofahl <stefan@pofahl.de>
    
    * file: .env 'BASIL_RESET_POSTGRES_CLUSTERS' value is now 0
    
    Signed-off-by: stefan_pofahl <stefan@pofahl.de>
    
    * BASIL Deploy Scripts for Apache2 Server
    
    - add transfer parameter to functions
    - for integer numbers use (( number < 0 )) instead of [[ "" .lt "0" ]]
    - use affix underscore "_" for local variables inside functions
    - remove non-printable chars like '\n' or '\r'
    
    Signed-off-by: stefan_pofahl <stefan@pofahl.de>
    
    ---------
    
    Signed-off-by: stefan_pofahl <stefan@pofahl.de>
    Co-authored-by: Luigi <63158371+pellecchialuigi@users.noreply.github.com>

--- deploy/apache/common.sh
+++ deploy/apache/common.sh
@@ -3,100 +3,117 @@
 source .env
 
 ## --- If $CLONEBASIL == 1, perform a fresh clone.
-CLONEBASIL="${BASIL_GITCLONE:-1}"
+CLONEBASIL=${BASIL_GITCLONE:-1}
 ## --- get current version (HEAD) $GITGOBACK == 0 or go back in time > 0
 ## --- $GITGOBACK < 0: take specified time $COMMITBEFORE to determine the value of $GITGOBACK
-GITGOBACK="${BASIL_GITGOBACK:-0}"
+GITGOBACK=${BASIL_GITGOBACK:-0}
 # --- Dates according to ISO8601, "JJJJ-MM-DD ..."
 TOMORROWDATE=$(date -d 'tomorrow 08:00' '+%Y-%m-%d %H:%M:%S %z')
 COMMITBEFORE="${BASIL_COMMITBEFORE:-$TOMORROWDATE}"
 
+## --- Functions ---------------------------------------------------------------------------------------
 clone_basil_repo_if_needed() {
-  # --- check if BASIL repository should be cloned ---------------------------------------------------
-  if [ $CLONEBASIL -eq 0 ]; then
-    # rm -R $BASIL_BUILD_DIR
-      if ! [ -d $BASIL_BUILD_DIR ]; then
-          echo "---  BASIL repository not found! Repository will be cloned!  ---"
-          git clone $BASIL_REPOSITORY $BASIL_BUILD_DIR
-      else
-          echo "---  Keep current version of BASIL.  --------------------------------------------"$'\n'
-      fi
-  else
-      echo $'\n'"--- re-clone BASIL git repository. -----------------------------------"
-      if [ -d $BASIL_BUILD_DIR ]; then
-          echo ---  re-establish folder $BASIL_BUILD_DIR    -------------------
-          rm -R $BASIL_BUILD_DIR
-      fi
-      mkdir -p $BASIL_BUILD_DIR
-      ## --- clone git repository --------------------------------------------
-      git clone $BASIL_REPOSITORY $BASIL_BUILD_DIR
-      echo $'\n'"=== END git clone =============================================================="$'\n'
-  fi
+    local _BASIL_REPOSITORY="${1}"
+    local _BASIL_BUILD_DIR="${2}"
+    local _CLONEBASIL=${3//[^0-9-]/}
+    local _GITGOBACK=${4//[^0-9-]/}
+    local _COMMITBEFORE="${5}"
+    echo "DBG: --- Function: clone_basil_repo_if_needed() --- \$_CLONEBASIL:  ${_CLONEBASIL} ----"
+    # --- check if BASIL repository should be cloned ---------------------------------------------------
+    if (( _CLONEBASIL = 0 )); then
+        echo "DBG: --- Function: clone_basil_repo_if_needed()  if [ $_CLONEBASIL -eq 0 ]; then ----"
+        # rm -R $_BASIL_BUILD_DIR
+        if ! [ -d $_BASIL_BUILD_DIR ]; then
+            echo "---  BASIL repository not found! Repository will be cloned!  ---"
+            git clone $_BASIL_REPOSITORY $_BASIL_BUILD_DIR
+        else
+            echo "---  Keep current version of BASIL.  --------------------------------------------"$'\n'
+        fi
+    else
+        echo $'\n'"--- re-clone BASIL git repository. -----------------------------------"
+        if [ -d $_BASIL_BUILD_DIR ]; then
+            echo ---  re-establish folder $_BASIL_BUILD_DIR    -------------------
+            rm -R $_BASIL_BUILD_DIR
+        fi
+        mkdir -p $_BASIL_BUILD_DIR
+        ## --- clone git repository --------------------------------------------
+        git clone $_BASIL_REPOSITORY $_BASIL_BUILD_DIR
+        echo $'\n'"--- END git clone -------------------------------------------------------------"$'\n'
+    fi
 
-  # --- Decide which mode to find the correct commit of BASIL go back in time or in commits / HEADS
-  cd "$BASIL_BUILD_DIR" || { echo "Error: Folder $BASIL_BUILD_DIR not found!"; exit 1; }
-  if [ $GITGOBACK -lt 0 ]; then
-      # --- validate correct time format of $COMMITBEFORE
-      if [[ ! "$COMMITBEFORE" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}\ [0-9]{2}:[0-9]{2}:[0-9]{2}\ [+-][0-9]{4}$ ]]; then
-          echo "Error: Invalid date format: $COMMITBEFORE"$'\n'; exit 1;
-      fi
-      # --- Check if latest commit date is after time: $COMMITBEFORE
-      latest_commit_date=$(git log -1 --format=%cd --date=format:%s)
-      commit_before_date=$(date -d "$COMMITBEFORE" +%s 2>/dev/null)
-      if [ "$commit_before_date" -gt "$latest_commit_date" ]; then
-          echo "Warning: Date $COMMITBEFORE is after the latest commit"
-          echo "This will return the latest commit (HEAD)"
-          # Optional: exit 1 falls gewünscht
-      fi
-      echo "--- Go back to commit that lies before:  $COMMITBEFORE  ---"
-      # --- get the latest commit that is strictly before the reference date
-      ref_commit=$(git rev-list -1 --before="$COMMITBEFORE" HEAD)
-      if [ -z "$ref_commit" ]; then
-          echo "No commit found before $COMMITBEFORE"
-          exit 1
-      fi
-      # --- set $GITGOBACK correctly to catch last commit that lies before $COMMITBEFORE
-      GITGOBACK=$(git rev-list --count "$ref_commit"..HEAD)
-  fi
-  # ---
-  TOTALNUMBEROFCOMMITS=$(git rev-list --count HEAD)
-  if [ $GITGOBACK -gt $TOTALNUMBEROFCOMMITS ]; then
-      echo $'\n'" ----  GITGOBACK = $GITGOBACK too high, maximum is $TOTALNUMBEROFCOMMITS  ----"$'\n'
-      echo $'\n'"=========================================================================="$'\n'
-      exit 1;
-  fi
 
-  # --- Check if it is a "merge commit" --------------------------------------------------------------
-  if ! git revert --no-commit HEAD~$GITGOBACK..HEAD 2>/dev/null; then
-      echo $'\n'"=========================================================================="$'\n'
-      echo "---   Error: git revert failed likely due to merge commits or other issues."
-      echo "---   Specify another commit! --- Maybe Refresh local copy of repository. "
-      echo "---   Specified or determined GITGOBACK is $GITGOBACK. --- Execution terminates now! ---"
-      echo $'\n'"=========================================================================="$'\n'
-  fi
+    # --- Decide which mode to find the correct commit of BASIL go back in time or in commits / HEADS
+    echo "--- change directory to: ${_BASIL_BUILD_DIR} --------------------------------------------"
+    printf 'DBG: $_GITGOBACK <%q>\n' "${_GITGOBACK}" # check if non-pritable chars (\r \n) exist
+    cd "${_BASIL_BUILD_DIR}" || { echo "Error: Folder $_BASIL_BUILD_DIR not found!"; exit 1; }
+    if (( _GITGOBACK < 0 )); then
+        # --- validate correct time format of $_COMMITBEFORE
+        if [[ ! "$_COMMITBEFORE" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}\ [0-9]{2}:[0-9]{2}:[0-9]{2}\ [+-][0-9]{4}$ ]]; then
+            echo "Error: Invalid date format: $_COMMITBEFORE"$'\n'; exit 1;
+        fi
+        # --- Check if latest commit date is after time: $_COMMITBEFORE
+        latest_commit_date=$(git log -1 --format=%cd --date=format:%s)
+        commit_before_date=$(date -d "$_COMMITBEFORE" +%s 2>/dev/null)
+        if [ "$commit_before_date" -gt "$latest_commit_date" ]; then
+            echo "Warning: Date $_COMMITBEFORE is after the latest commit"
+            echo "This will return the latest commit (HEAD)"
+            # Optional: exit 1 falls gewünscht
+        fi
+        echo "--- Go back to commit that lies before:  $_COMMITBEFORE  ---"
+        # --- get the latest commit that is strictly before the reference date
+        ref_commit=$(git rev-list -1 --before="$_COMMITBEFORE" HEAD)
+        if [ -z "$ref_commit" ]; then
+            echo "No commit found before $_COMMITBEFORE"
+            exit 1
+        fi
+        # --- set $_GITGOBACK correctly to catch last commit that lies before $_COMMITBEFORE
+        _GITGOBACK=$(git rev-list --count "$ref_commit"..HEAD)
+    fi
+    # ---
 
-  # --- open with specified or determined HEAD=$GITGOBACK --------------------------------------------
-  # echo "TOTALNUMBEROFCOMMITS:  $TOTALNUMBEROFCOMMITS"$'\n'
-  COMMITDATE=$(git show -s --format=%ci HEAD~$GITGOBACK)
-  ## --- Get the original commit message (subject + body)
-  ORIGMSG=$(git show -s --format=%B HEAD~$GITGOBACK)
-  ## --- Extract commit number if present, don’t exit if missing
-  COMMITNUMBER=$(grep -oP '#\K[0-9]+' <<<"$ORIGMSG" || true)
-  ## --- Combine them into one commit message (quote properly!)
-  COMMITMESSAGE="Revert of commit HEAD~${GITGOBACK} from: ${COMMITDATE}"
-  echo "---  Commit message:  -------------------------------------------------------------"$'\n'
-  echo ${ORIGMSG}
-  echo $'\n'"------------------------------------------------------------------------------"$'\n'
-  # echo "DBG --- This is line $LINENO, next: git commit -m "
+    TOTALNUMBEROFCOMMITS=$(git rev-list --count HEAD)
+    if (( _GITGOBACK > TOTALNUMBEROFCOMMITS )); then
+        echo $'\n'" ----  _GITGOBACK = $_GITGOBACK too high, maximum is $TOTALNUMBEROFCOMMITS  ----"$'\n'
+        echo $'\n'"=========================================================================="$'\n'
+        exit 1;
+    fi
 
-  ## --- revert and commit, if $GITGOBACK is greater then 0: -----------------------------------------
-  if [ $GITGOBACK -gt 0 ]; then
-      echo " ---  GITGOBACK = $GITGOBACK is greater then 0:  --- "
-      # --- Following commands should not stop the script execution, true = Exit-Code 0
-      git revert --no-commit HEAD~$GITGOBACK..HEAD 2>/dev/null || true
-      git commit -m "$COMMITMESSAGE"  2>/dev/null || true
-  fi
+    # --- Check if it is a "merge commit" --------------------------------------------------------------
+    if ! git revert --no-commit HEAD~$_GITGOBACK..HEAD 2>/dev/null; then
+        echo $'\n'"=========================================================================="$'\n'
+        echo "---   Error: git revert failed likely due to merge commits or other issues."
+        echo "---   Specify another commit! --- Maybe Refresh local copy of repository. "
+        echo "---   Specified or determined _GITGOBACK is $_GITGOBACK. --- Execution terminates now! ---"
+        echo $'\n'"=========================================================================="$'\n'
+    fi
+
+    # --- open with specified or determined HEAD=$_GITGOBACK --------------------------------------------
+    # echo "TOTALNUMBEROFCOMMITS:  $TOTALNUMBEROFCOMMITS"$'\n'
+    COMMITDATE=$(git show -s --format=%ci HEAD~$_GITGOBACK)
+    ## --- Get the original commit message (subject + body)
+    ORIGMSG=$(git show -s --format=%B HEAD~$_GITGOBACK)
+    ## --- Extract commit number if present, don’t exit if missing
+    COMMITNUMBER=$(grep -oP '#\K[0-9]+' <<<"$ORIGMSG" || true)
+    ## --- Combine them into one commit message (quote properly!)
+    COMMITMESSAGE="Revert of commit HEAD~${_GITGOBACK} from: ${COMMITDATE}"
+    echo "---  Commit message:  -------------------------------------------------------------"$'\n'
+    echo Commit Date: ${COMMITDATE}
+    echo ${ORIGMSG}
+    echo $'\n'"------------------------------------------------------------------------------"$'\n'
+    # echo "DBG --- This is line $LINENO, next: git commit -m "
+
+    ## --- revert and commit, if $_GITGOBACK is greater then 0: -----------------------------------------
+    if (( _GITGOBACK > 0 )); then
+        echo " ---  _GITGOBACK = $_GITGOBACK is greater then 0:  --- "
+        # --- Following commands should not stop the script execution, true = Exit-Code 0
+        git revert --no-commit HEAD~$_GITGOBACK..HEAD 2>/dev/null || true
+        git commit -m "$COMMITMESSAGE"  2>/dev/null || true
+    fi
+    #---
+    echo $'\n'"=== END function <clone_basil_repo_if_needed>  ===================================="$'\n'
+    return 0
 }
+
 
 install_package() {
   PACKAGE=$1
@@ -111,19 +128,36 @@ install_package() {
 }
 
 add_port_to_listen() {
-    ## --- Add port to apache configuration
-    NEW_PORT="$1"
-    APACHE_PORTS=/etc/apache2/ports.conf
-    ## --- find "Listen XX" (XX = port number), I have tried several variants, this works fine
-    if grep -vE '^[[:space:]]*#' "$APACHE_PORTS" | grep -Eq "^[[:space:]]*Listen\b[[:space:]]+$NEW_PORT\b"; then
-    # if grep -Eq "^[[:space:]]*Listen[[:space:]]+$NEW_PORT([[:space:]]|$)" "$APACHE_PORTS"; then
-	echo "'Listen ${NEW_PORT}' already present in $APACHE_PORTS"
-    else
-	echo "New Port ${NEW_PORT} not specified in '$APACHE_PORTS'!"
-	echo "Listen ${NEW_PORT}" | sudo tee -a /etc/apache2/ports.conf
-	echo "Added 'Listen ${NEW_PORT}' to '$APACHE_PORTS'"
+    local file_path=$(echo "$1" | tr -cd '[:print:]')     # Parameter A: file path
+    local search_string=$(echo "$2" | tr -cd '[:print:]') # Parameter B: string
+
+    # Validate parameters
+    if [[ -z "$file_path" || -z "$search_string" ]]; then
+        echo "Usage: add_port_to_listen <file-path> <string>" >&2
+        return 1
     fi
+
+    if [[ ! -f "$file_path" ]]; then
+        echo "Error: File not found: $file_path" >&2
+        return 1
+    fi
+
+    #: Check if Listen <port> already exists (any whitespace)
+    if grep -Eq "^[[:space:]]*Listen[[:space:]]+$search_string([[:space:]]*|$)" "$file_path"; then
+        echo "Listen $search_string already present"
+        return 0
+    else
+        #: Append safely
+        # echo "Listen $search_string" >> "$file_path"
+        echo "Listen ${search_string}" | sudo tee -a "$file_path" > /dev/null
+        echo "Added: Listen $search_string"
+    fi
+    echo "---------------------------------------------------------------------------------"
+    echo "****  END add_port_to_listen                                                 ****"
+    echo "---------------------------------------------------------------------------------"
+    return 0
 }
+
 
 rcpdir() {
     local source_dir="$1"
